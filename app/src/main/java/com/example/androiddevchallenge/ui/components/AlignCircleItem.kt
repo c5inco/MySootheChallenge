7777291e40c23c4e72165aabfa32c0b5cb38e469
Chris Sinco <csinco.design@gmail.com>
2021-08-23 21:59:53 -0700

    Add AlignCircleItem component

---
+++ app/src/main/java/com/example/androiddevchallenge/ui/components/AlignCircleItem.kt
@@ -0,0 +1,57 @@
+package com.example.androiddevchallenge.ui.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.androiddevchallenge.R
+import com.example.androiddevchallenge.ui.theme.MyTheme
+
+@Composable
+fun AlignCircleItem(
+    title: String,
+    imageId: Int
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painterResource(id = imageId),
+            contentDescription = "Image for $title",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape),
+        )
+        Text(
+            title,
+            style = MaterialTheme.typography.h3,
+            modifier = Modifier.paddingFromBaseline(top = 24.dp)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun AlignCircleItemPreview() {
+    MyTheme {
+        Surface {
+            AlignCircleItem(
+                title = "Inversions",
+                imageId = R.drawable.inversions
+            )
+        }
+    }
+}
